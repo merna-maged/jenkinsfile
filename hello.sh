3ee05d277d1f5ce91435ca9fd941729a54902797
merna-maged <mernamagedmonir@gmail.com>
2025-12-24 15:31:17 +0200

    Add hello.sh script to print a greeting message

---
+++ hello.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "****************************************"
+echo "Hello! This is running from a shell script file."
+echo "Task 09 is completed successfully!"
+echo "****************************************"
