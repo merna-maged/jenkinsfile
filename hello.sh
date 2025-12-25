67dc0dd7dfb1edbcd7ea75f61d5a057479fafb9e
merna-maged <mernamagedmonir@gmail.com>
2025-12-25 13:26:18 +0200

    Update task completion messages in hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "****************************************"
 echo "Hello! This is running from a shell script file."
-echo "Task 09 is completed successfully!"
+echo "Task 11 is completed successfully!"
+echo "Testing Branch Dev: Task 11 is working!"
 echo "****************************************"
