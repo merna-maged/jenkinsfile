1275c755baffcf0ac8383acb5ad99c1d429af85b
merna-maged <mernamagedmonir@gmail.com>
2025-12-29 11:26:33 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-echo "****************************************"
-echo "Hello! This is running from a shell script file."
-echo "Task 09 is completed successfully!"
-echo "****************************************"
+echo "hello from jenkins and docker"
